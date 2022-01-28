416c52f4c0451a5c0147b62bb44336b84e3f3c11
habadjian <habadjian@yahoo.com>
2022-01-27 21:02:08 -0800

    Infinite loop message
    Failed test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,18 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void getLinksTester() throws IOException{
+       // getLinks(markdown)
+        assertEquals(0, 0);
     }
 }
--- test2.md
+++ test2.md
@@ -1,1 +1,6 @@
 [link Uno!](https://docs.google.com)
+[hiiii](special.html)
+[a link!](12345)
+[a link!](https://ucsd-cse15l-w22.github.io/week/week3/)
+[[a link!]]((https://forms.gle/Pz7VAp6tFyuQJD9W6))
+[a link!](https://(forms).gle/Pz7VAp6tFyuQJD9W6)
