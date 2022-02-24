71562cc61640c6ea07aca66690d837670b7c9d99
habadjian <habadjian@yahoo.com>
2022-02-23 19:34:14 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -20,6 +20,7 @@ public class MarkdownParse {
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            //extra comment
             //System.out.println(currentIndex);
         }
         return toReturn;
