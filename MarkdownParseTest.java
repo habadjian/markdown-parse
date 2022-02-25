8abbb9c7df1d55ed678a126dcc96c301ec45781b
habadjian <habadjian@yahoo.com>
2022-02-24 21:39:28 -0800

    added the tests snippet1, 2, and 3

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -17,10 +17,36 @@ public class MarkdownParseTest {
 
     @Test
     public void getLinksTester() throws IOException{
-        Path fileName = Path.of("test-file.md");
-	    String contents = Files.readString(fileName);
-        assertEquals(MarkdownParse.getLinks(contents), new ArrayList<String>(List.of("https://something.com", "some-page.html")));
+        //Path fileName = Path.of("./test-file.md");
+	    //String contents = Files.readString(fileName);
+        //assertEquals(MarkdownParse.getLinks(contents), new ArrayList<String>(List.of("https://something.com", "some-page.html")));
     }
+
+    @Test
+    public void snippet1() throws IOException{
+        Path fileName = Path.of("/Users/heikoabadjian/Documents/GitHub/markdown-parse/snippet1.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("`google.com","google.com","ucsd.edu"),links);
+    }
+
+    @Test
+    public void snippet2() throws IOException{
+        Path fileName = Path.of("/Users/heikoabadjian/Documents/GitHub/markdown-parse/snippet2.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("a.com", "a.com((", "example.com"),links);
+    }
+
+    @Test
+    public void snippet3() throws IOException{
+        Path fileName = Path.of("/Users/heikoabadjian/Documents/GitHub/markdown-parse/snippet3.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("https://ucsd-cse15l-w22.github.io/"),links);
+    }
+
+
 
     
 }
